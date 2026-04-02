cbfdd25fe6500ee28ae01fdba4c9b24a40b73cb8
Mateus <mateusoares10hp@gmail.com>
2026-04-02 16:05:35 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Simples',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 19, 158, 23),
+        ),
+      ),
+      home: const TelaPrincipal(),
+    );
+  }
+}
+
+class TelaPrincipal extends StatelessWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text('Atividade GitHub'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Olá, mundo!',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('botao'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
